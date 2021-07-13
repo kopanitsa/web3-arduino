c4ad393b3a11ab3fc97bd5748d05e647b93bd2d8
joseluu <jose.luu@gmail.com>
2021-07-13 15:55:08 +0200

    fix compilation

--- examples/eth_send/eth_send.ino
+++ examples/eth_send/eth_send.ino
@@ -1,6 +1,9 @@
+#include <Arduino.h>
 #include <WiFi.h>
 #include <Web3.h>
 #include <Contract.h>
+#include <esp_system.h>
+#include <esp_log.h>
 
 #define USE_SERIAL Serial
 
@@ -15,9 +18,18 @@ const char PRIVATE_KEY[] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
                             0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
                             0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
                             0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00 };
-Web3 web3(INFURA_HOST, INFURA_PATH);
+Web3 web3(new std::string(INFURA_HOST),new std::string( INFURA_PATH));
 
 void eth_send_example();
+
+String getMacAddress() {
+  uint8_t baseMac[6];
+  // Get MAC address for WiFi station
+  esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
+  char baseMacChr[18] = {0};
+  sprintf(baseMacChr, "%02X:%02X:%02X:%02X:%02X:%02X", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
+  return String(baseMacChr);
+}
 
 void setup() {
     USE_SERIAL.begin(115200);
@@ -30,6 +42,9 @@ void setup() {
 
     WiFi.begin(ENV_SSID, ENV_WIFI_KEY);
 
+    String mac = getMacAddress();
+    USE_SERIAL.println(mac);
+    
     // attempt to connect to Wifi network:
     while (WiFi.status() != WL_CONNECTED) {
         Serial.print(".");
@@ -39,6 +54,7 @@ void setup() {
 
     USE_SERIAL.println("Connected");
 
+
     eth_send_example();
 }
 
@@ -47,19 +63,19 @@ void loop() {
 }
 
 void eth_send_example() {
-    Contract contract(&web3, CONTRACT_ADDRESS);
+    Contract contract(&web3, new std::string(CONTRACT_ADDRESS));
     contract.SetPrivateKey((uint8_t*)PRIVATE_KEY);
-    uint32_t nonceVal = (uint32_t)web3.EthGetTransactionCount((char *)MY_ADDRESS);
+    uint32_t nonceVal = (uint32_t)web3.EthGetTransactionCount(new std::string(MY_ADDRESS));
 
     uint32_t gasPriceVal = 141006540;
     uint32_t  gasLimitVal = 3000000;
-    uint8_t toStr[] = CONTRACT_ADDRESS;
-    uint8_t valueStr[] = "0x00";
+    std::string toStr(CONTRACT_ADDRESS);
+    std::string valueStr("0x00");
     uint8_t dataStr[100];
     memset(dataStr, 0, 100);
     string func = "set(uint256)";
     string p = contract.SetupContractData(&func, 123);
     string result = contract.SendTransaction(nonceVal, gasPriceVal, gasLimitVal, &toStr, &valueStr, &p);
 
-    USE_SERIAL.println(result);
+    USE_SERIAL.println(result.c_str());
 }
